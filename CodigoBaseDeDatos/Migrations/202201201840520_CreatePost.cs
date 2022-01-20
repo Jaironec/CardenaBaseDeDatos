d01cafd4405ec142ab339b8f1977003344400943
Jairon Jose Cardena Barre <JJCARDENA@pucese.edu.ec>
2022-01-20 13:48:08 -0500

    Agregar archivos de proyecto.

---
+++ CodigoBaseDeDatos/Migrations/202201201840520_CreatePost.cs
@@ -0,0 +1,28 @@
+﻿namespace CodigoBaseDeDatos.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatePost : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        fecha_de_publicación = c.DateTime(nullable: false),
+                        Titulo = c.String(),
+                        Cuerpo = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
---
+++ CodigoBaseDeDatos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoBaseDeDatos
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public DateTime fecha_de_publicación { get; set; }
+        public string Titulo { get; set; }
+        public string Cuerpo { get; set; }
+
+    }
+    public class blogDbContext: DbContext
+    {
+        public DbSet<Post> posts { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
